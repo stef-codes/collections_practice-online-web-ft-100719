39a9ea2a202424f22890904325f78b06d7712759
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-11 20:37:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,7 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect do {|element, index| 
+  array.each_with_index.collect {|element, index| 
     if element[1] == element
     else element + "s"}
   end 
